82a711491ff7404fdd679c809f475fc44e5e4829
Fatemeh HajgedaAli <fatemeh.hajgedaali1996@gmail.com>
2026-09-01 11:29:24 +0330

    debug

---
+++ src/services/emailService.js
@@ -0,0 +1,8 @@
+export async function sendAppointmentEmail(data) {
+  console.log("Appointment email data:", data);
+
+  // TODO: connect to your email API/service
+  return {
+    success: true,
+  };
+}
